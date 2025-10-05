27f46d619e6fb3905f024c2a16eaa3377634f2ad
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-10-05 16:04:40 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS29.java
+++ jobsheet5/ifCetakKRS29.java
@@ -15,6 +15,8 @@ public static void main(String[] args) {
     }else{
         System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
     }
-
+ System.out.println(uktLunas ? 
+            "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" : 
+            "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
 }
 }
