7d02c2b54037def04f5ce76639e6c8131fb1e692
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-10-23 12:28:25 +0700

    Modifikasi Percobaan 1

--- jobsheet7/SiakadFor29.java
+++ jobsheet7/SiakadFor29.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SiakadFor29 {
     public static void main(String[] args) {
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahlulus = 0, jumlahtidaklulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             Scanner scanner = new Scanner(System.in);
@@ -17,8 +18,19 @@ public class SiakadFor29 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if(nilai >= 60){
+                jumlahlulus++;
+            } else {
+                jumlahtidaklulus++;
+            }
+
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + jumlahlulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + jumlahtidaklulus);
+
+        
     }
 }
