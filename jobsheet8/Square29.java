1b677581d6cac3c4bea4c71979ad264b6d601f27
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-11-01 09:45:38 +0700

    Percobaan 2

--- jobsheet8/Square29.java
+++ jobsheet8/Square29.java
@@ -6,6 +6,7 @@ public class Square29 {
         Scanner sc = new Scanner (System.in);
         System.out.println("Masukkan nilai n: ");
         int n = sc.nextInt();
+
         for (int iOuter = 1; iOuter <= n; iOuter++) {
             for (int i = 1; i <= n; i++) {
                 System.out.print("*");
