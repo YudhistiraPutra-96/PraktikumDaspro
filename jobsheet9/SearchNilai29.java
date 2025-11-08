72850712c8eb539aaac2e4887967b26fb01513fe
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-11-08 11:54:51 +0700

    Modifikasi Percobaan 4

--- jobsheet9/SearchNilai29.java
+++ jobsheet9/SearchNilai29.java
@@ -1,19 +1,50 @@
 package jobsheet9;
+import java.util.Scanner;
 
 public class SearchNilai29 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+        int key = 0;
+        int hasil = -1;
+        int banyaknilai;
+        int i ;
+
+        System.out.println("Masukkan banyaknya nilai yang akan diinput : ");
+        banyaknilai = scanner.nextInt();
+
+        int[] arrNilai = new int[banyaknilai];
+
+        for (i = 0; i < banyaknilai; i++) {
+            System.out.print("Masukkan nilai ke-" + (i+1) + " : ");
+            arrNilai[i] = scanner.nextInt();
+            
+            
+            // if (key == arrNilai[i]) {
+            //     hasil = i;
+            //     break;
+            // }
+            
+        
+        // System.out.println();
+        // System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        }// System.out.println();
+        System.out.println("Masukkan nilai yang ingin dicari : ");
+        key = scanner.nextInt();
+
+        for(i = 0; i < banyaknilai; i++){
+            if(key == arrNilai[i]){
                 hasil = i;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+
+        
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil+1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        
+        }
     }
 }
