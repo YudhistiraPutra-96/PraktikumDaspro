d3ce1a4540a4aa5fbe75e57e4e074e18765b9c67
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-11-15 08:58:06 +0700

    Modifikasi Percobaan 2

--- jobsheet10/BioskopWithScanner29.java
+++ jobsheet10/BioskopWithScanner29.java
@@ -7,25 +7,81 @@ public class BioskopWithScanner29 {
         String [][] penonton = new String [4][2];
         String nama;
         int baris, kolom;
-        String next;
+        String next = "";
+        int pilihan;
+        
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("=== MENU BIOSKOP ===");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            if(pilihan == 1){
+                while (true) {
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                while (true) {    
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    if (baris < 1 || baris > 4) {
+                    System.out.println("Baris tidak tersedia, Masukkan baris antara 1-4");
+                    continue;
+                    }
+                    break;
+                }
 
-            if (next.equalsIgnoreCase("n")) {
+                while (true) {
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    if (kolom < 1 || kolom > 2) {
+                        System.out.println("Kolom tidak tersedia, Masukkan kolom antara 1-2");
+                        continue;
+                    }break;                
+                }sc.nextLine();
+
+                if (penonton[baris-1][kolom-1] != null) {
+                    System.out.println("Kursi telah terisi oleh penonton lain");
+                    continue;
+                }    
+                    penonton[baris-1][kolom-1] = nama;
+
+                    System.out.print("Input penonton lainnya? (y/n): ");
+                    next = sc.nextLine();
+
+                    if (next.equalsIgnoreCase("n")) {
+                        break;
+                    }
+                }
+
+
+            }if (pilihan == 2) {
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.print("Baris " + (i+1) + ": ");
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        if (penonton[i][j] == null) {
+                            System.out.print("***");
+                        } else {
+                            System.out.print(penonton[i][j]);
+                        }
+                        if (j < penonton[i].length - 1) {
+                            System.out.print(", ");
+                        }
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            
+
+            } else if (pilihan == 3) {
+                System.out.println("Terima kasih telah menggunakan aplikasi bioskop.");
                 break;
             }
-        }
+        }            
     }
 }
