d3af610f138d7d1f2b7c0d1d0e35c8d9a7f4801a
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-11-29 09:37:24 +0700

    Percobaan 2

--- jobsheet11/Kafe29.java
+++ jobsheet11/Kafe29.java
@@ -2,14 +2,24 @@ package jobsheet11;
 
 public class Kafe29 {
     public static void main(String[] args) {
-        System.out.println("===== MENU RESTO KAFE =====");
-        System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappucino - Rp 20,000");
-        System.out.println("3. Latte - Rp 22,000");
-        System.out.println("4. Teh Tarik - Rp 12,000");
-        System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("====================================");
-        System.out.println("Silahkan pilih menu yang Anda inginkan.");
+       Menu("Andi", true);
     }
+
+    public static void Menu(String namePelanggan, boolean isMember) {
+    System.out.println("Selamat datang, " + namePelanggan + "!");
+
+    if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+    }
+
+    System.out.println("====== MENU RESTO KAFE =====");
+    System.out.println("1. Kopi Hitam - Rp 15,000");
+    System.out.println("2. Cappuccino - Rp 20,000");
+    System.out.println("3. Latte - Rp 22,000");
+    System.out.println("4. Teh Tarik - Rp 12,000");
+    System.out.println("5. Roti Bakar - Rp 10,000");
+    System.out.println("6. Mie Goreng - Rp 18,000");
+    System.out.println("===================");
+    System.out.println("Silakan pilih menu yang Anda inginkan.");
+}
 }
