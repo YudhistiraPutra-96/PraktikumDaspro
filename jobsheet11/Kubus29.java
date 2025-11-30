637b986dbcabeb1e6cd1dc59bd900dc786f414bd
YudhistiraPutra-96 <hartantoyudhis06@gmail.com>
2025-11-30 15:48:03 +0700

    Tugas1

---
+++ jobsheet11/Kubus29.java
@@ -0,0 +1,21 @@
+package jobsheet11;
+
+import java.util.Scanner;
+public class Kubus29 {
+    public static void main(String[] args) {
+     
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan Panjang Sisi Kubus yang anda mau hitung : ");
+        double sisi = sc.nextInt();
+    
+        Kubus(sisi);
+    }
+    static void Kubus(double sisi){
+        double volume = sisi * sisi * sisi;
+        System.out.println("Volume Kubus : " + volume);
+        double luaspermukaan = 6 * sisi * sisi;
+        System.out.println("Luas Permukaan Kubus : " + luaspermukaan);
+        
+        
+    }
+}
